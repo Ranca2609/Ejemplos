d722e3bf24fafbaf51d0a2b6e3c40b7f3c454a0a
Rangel2609 <3005653640101@ingenieria.usac.edu.gt>
2022-01-08 01:54:25 -0600

    08/01/2021

---
+++ Python/Lectura y Escritura de un XML/Constructor.py
@@ -0,0 +1,11 @@
+class Empleado:
+    def __init__(self, id, nombre, username, password):
+        self.id = id
+        self.nombre = nombre
+        self.username = username 
+        self.password = password
+        
+
+        
+        
+        
---
+++ Python/Lectura y Escritura de un XML/Lectura.py
@@ -0,0 +1,39 @@
+from xml.etree import ElementTree as ET 
+from  Constructor import Empleado
+
+tree = ET.parse('Archivo.xml')
+root = tree.getroot()
+Array = []
+for Elemento in root:
+    id = Elemento.attrib['id']
+    for Subelemento in Elemento:
+        if Subelemento.tag == 'nombre':
+            Nombre = Subelemento.text.strip(' ')
+        if Subelemento.tag == 'username':
+            Username = Subelemento.text.strip(' ')
+        if Subelemento.tag == 'password':
+            Password = Subelemento.text.strip(' ')
+    Array.append(Empleado(id, Nombre, Username, Password))
+
+for i in Array:
+    print('Id: '+i.id + '\nNombre: '+ i.nombre + "\nUsername: " + i.username
+          + '\nPassword:' + i.password)
+
+'''
+----------Salida en Consola----------
+Id: 1
+Nombre: Angel Daniel
+Username: AD
+Password:123456
+Id: 2
+Nombre: Jose Pimentel
+Username: JP
+Password:ejemplo123@
+'''
+        
+            
+        
+    
+
+    
+    
