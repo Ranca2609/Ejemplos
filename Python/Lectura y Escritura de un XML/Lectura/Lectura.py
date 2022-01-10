79127133b675838f6c1c74abcd44e968cdf6458d
Rangel2609 <3005653640101@ingenieria.usac.edu.gt>
2022-01-10 14:04:10 -0600

    10/01/2022

--- Python/Lectura y Escritura de un XML/Lectura/Lectura.py
+++ Python/Lectura y Escritura de un XML/Lectura/Lectura.py
@@ -1,6 +1,6 @@
 from xml.etree import ElementTree as ET 
 from  Constructor import Empleado
-
+from xml.dom import minidom
 
 #----------Lectura de un XML----------------
 
@@ -35,11 +35,33 @@ Password:ejemplo123@
 '''
         
 #----------Escritura de un XML---------------
-# Se utiliza la libreria Minidoom para la escritura de un xml
+# Se utiliza la libreria Minidom para la escritura de un xml
 
-   
-        
+document = minidom.Document()
+root = document.createElement('empresa')
+document.appendChild(root)
+for i in Array: 
+    Empleado = document.createElement('empleado')  
+    Empleado.setAttribute("id", i.id) 
+    root.appendChild(Empleado)
+    #----------------------------------------------------------
+    Nombre = document.createElement('nombre')
+    Nombre.appendChild(document.createTextNode(i.nombre))  
+    Empleado.appendChild(Nombre)
+    #----------------------------------------------------------
+    Username = document.createElement('username')
+    Username.appendChild(document.createTextNode(i.username))  
+    Empleado.appendChild(Username)
+    #----------------------------------------------------------
+    Password = document.createElement('password')
+    Password.appendChild(document.createTextNode(i.password))  
+    Empleado.appendChild(Password)
     
+xml = document.toprettyxml(indent='\t', newl='\n', encoding='utf-8') 
+xml = xml.decode('utf-8')
+Salida = open('C:/Users/Carlos Rangel/Documents/GitHub/Ejemplos/Python/Lectura y Escritura de un XML/Lectura/Salida.xml', 'w')
+Salida.write(xml)
+Salida.close()
 
     
     
